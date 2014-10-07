ac40b2c83bf49434285ae6b63f33f509cbe0d62e
Frantisek Uhrecky <frantisek.uhrecky@gmail.com>
2014-10-08 01:08:54 +0200

    added licence header

--- lib/src/operations.c
+++ lib/src/operations.c
@@ -1,3 +1,24 @@
+/**
+ This file is part of BitPunch
+ Copyright (C) 2014 Frantisek Uhrecky <frantisek.uhrecky[at]gmail.com>
+ Copyright (C) 2014 Andrej Gulyas <andrej.guly[at]gmail.com>
+ Copyright (C) 2014 Marek Klein  <kleinmrk[at]gmail.com>
+ Copyright (C) 2014 Filip Machovec  <filipmachovec[at]yahoo.com>
+ Copyright (C) 2014 Jozef Kudlac Uhrecky <kudalc.jozef[at]gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
