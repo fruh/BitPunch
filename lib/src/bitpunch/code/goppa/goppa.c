19a1067dbf4e70f41d6e99f16d395467529b490f
Marek Klein <kleinmrk@gmail.com>
2016-09-28 17:23:38 +0200

    Using constant number of steps in XGCD

--- lib/src/bitpunch/code/goppa/goppa.c
+++ lib/src/bitpunch/code/goppa/goppa.c
@@ -233,7 +233,7 @@ void BPU_goppaFindPolyAB(BPU_T_GF2_16x_Poly * a, BPU_T_GF2_16x_Poly * b,
     int end_deg = mod->deg / 2;
 
     BPU_gf2xPolyMalloc(&tmp, (tau->deg > mod->deg) ? tau->deg : mod->deg);
-    BPU_gf2xPolyExtEuclid(a, b, tmp, tau, mod, end_deg, math_ctx);
+    BPU_gf2xPolyExtEuclidC(a, tmp, b, mod, tau, end_deg, math_ctx);
     BPU_gf2xPolyFree(&tmp);
 }
 #endif // BPU_CONF_DECRYPTION
